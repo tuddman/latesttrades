a22ec5ec2e6e1a7642b08969baaa63d43163d6d0
tuddman <scott.tudd@gmail.com>
2014-01-29 14:52:46 -0500

    html > jade + index.html

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
+var https = require('https');
 var path = require('path');
 
 var app = express();
@@ -14,7 +15,7 @@ var app = express();
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -28,8 +29,14 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+//app.get('/', routes.index);
+
+
+app.get('/', function(req, res) {
+
+    res.render('index.html');
+});
+
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
